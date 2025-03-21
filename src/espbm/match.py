0bb23852c9afba96732bb83f1bd749227a348661
Tim Büchner <tim.buechner@uni-jena.de>
2025-03-21 16:55:40 +0100

    Introduce blinking prototype parameter optmization

--- src/espbm/match.py
+++ src/espbm/match.py
@@ -1,12 +1,16 @@
-__all__ = ["find_prototype", "match_found_intervals"]
+__all__ = ["find_prototype", "describe", "index_matching", "optim"]
+
+from typing import Any
+import warnings
 
 import numpy as np
 import stumpy
+from scipy import optimize
 
-def find_prototype(
-    ear_ts: np.ndarray, 
-    prototype: np.ndarray, 
-    max_prototype_distance: float=3.0):
+from espbm.manual import create_prototype
+
+
+def find_prototype(ear_ts: np.ndarray, prototype: np.ndarray, max_prototype_distance: float = 3.0):
     """
     Find occurrences of a prototype pattern within a time series.
 
@@ -19,37 +23,40 @@ def find_prototype(
     list: A list of intervals where the prototype pattern is found in the time series.
           Each interval is represented as [from, to, distance_to_prototype].
     """
+
     def threshold(D):
         return np.nanmax([np.nanmean(D) - max_prototype_distance * np.std(D), np.nanmin(D)])
-    
+
     matches = stumpy.match(prototype, ear_ts, max_distance=threshold)
     # sort the matches by index to get the original order
     matches = sorted(matches, key=lambda x: x[1])
-    
+
     intervals = []
     # transform the matches to be [from, to, distance_to_prototype]
     for match in matches:
         intervals.append([match[1], match[1] + len(prototype), match[0]])
-    
+
     return intervals
+
 
 def describe(matches: np.ndarray):
     """
     Prints information about the given matches array.
-    
+
     Parameters:
         matches (np.ndarray): An array of matches.
-    
+
     Returns:
         None
     """
     print(f"Contains {len(matches)} matches")
     print(f"Matches: {matches}")
 
+
 def index_matching(
     matches_l: np.ndarray,
     matches_r: np.ndarray,
-    max_match_distance: int=50,
+    max_match_distance: int = 50,
 ) -> tuple[list[np.ndarray], list[np.ndarray]]:
     """
     Perform index matching between two arrays of matches.
@@ -60,28 +67,127 @@ def index_matching(
         max_match_distance (int, optional): Maximum distance allowed between matches. Defaults to 50.
 
     Returns:
-        tuple[list[np.ndarray], list[np.ndarray]]: A tuple containing two lists of matches, 
-        where the first list corresponds to the matches from the left side and the second list 
+        tuple[list[np.ndarray], list[np.ndarray]]: A tuple containing two lists of matches,
+        where the first list corresponds to the matches from the left side and the second list
         corresponds to the matches from the right side.
 
     """
     start_idx_l = np.array(matches_l)[:, 0]
     start_idx_r = np.array(matches_r)[:, 0]
-    
+
     pairs = []
-    
+
     for i, idx_l in enumerate(start_idx_l):
         dists = np.abs(start_idx_r - idx_l)
-        
+
         min_dist = np.min(dists)
         min_argl = np.argmin(dists)
-        
+
         if min_dist < max_match_distance:
             # when there are two equal-dist matches, always keep the first one
             pairs.append([i, min_argl])
-            
+
     n_matches_l = [matches_l[i] for i, _ in pairs]
     n_matches_r = [matches_r[j] for _, j in pairs]
-    
-    # TODO return also the non-matches        
+
+    # TODO return also the non-matches
     return n_matches_l, n_matches_r
+
+
+def optim(
+    interval: np.ndarray,
+    prototype_params: tuple[float, float, float, float, float],
+) -> tuple[np.ndarray, tuple[float, float, float, float, float]]:
+    """
+    Optimize the blinking prototype parameters for a given interval.
+
+    Parameters:
+        interval (np.ndarray): The interval to optimize the prototype parameters for.
+        prototype_params (tuple[float, float, float, float, float]): The prototype parameters.
+
+    Returns:
+        tuple[np.ndarray, tuple[float, float, float, float, float]]: The optimized prototype and its parameters.
+    """
+
+    def optimize_prototype_fun(params: tuple, interval: np.ndarray):
+        """
+        Optimize the prototype function by minimizing the L1 norm between the interval and the prototype.
+        Args:
+            params (list or array-like): Parameters to create the prototype.
+            interval (list or array-like): The interval to compare against the prototype.
+        Returns:
+            float: The L1 norm (Manhattan distance) between the interval and the prototype.
+        """
+
+        prototype = create_prototype(params)
+        return np.linalg.norm(interval - prototype, ord=1)
+
+    with warnings.catch_warnings():
+        # we ignore the warnings from the optimization process
+        # as in really really rare cases on of the guesses might be out of bounds
+        # and the optimization process will still converge
+        warnings.simplefilter("ignore")
+        # Note: for now the ranges are hardcoded, but they are sufficient for the given data
+        ret = optimize.minimize(
+            optimize_prototype_fun,
+            prototype_params,
+            args=(interval,),
+            method="Powell",
+            bounds=[
+                (1.00, 20.0),  # sig1
+                (1.00, 20.0),  # sig2
+                (np.nanmin(interval), np.nanmax(interval)),  # baseline
+                (0.01, 0.75),  # prominance
+                (0.30, 0.50),  # apex_location
+            ],
+            options={"maxiter": 1000, "disp": False},
+        )
+    if not ret.success:
+        return None, None
+
+    optimized_params = ret.x
+    optimized_prototype = create_prototype(optimized_params)
+    return optimized_prototype, tuple(optimized_params)
+
+
+def interval_stats(
+    interval: np.ndarray,
+    prototype_params: tuple[float, float, float, float, float],
+) -> dict[str, Any]:
+    # lets compute some of the important statistics for the interval
+    stats: dict[str, Any] = {}
+
+    sig1, sig2, baseline, height, apex_location = prototype_params
+    apex_location = int(apex_location * len(interval))
+
+    stats["sig1"] = sig1
+    stats["sig2"] = sig2
+    stats["baseline"] = baseline
+    apex_score = interval[apex_location]
+    stats["apex_score"] = apex_score
+    stats["heights"] = height
+    stats["prominance"] = apex_score - baseline
+
+    stats["apex_location"] = apex_location
+    # get the score at apex
+    onset_x = int(apex_location - 2.5 * sig1)
+    offset_x = int(apex_location + 2.5 * sig2)
+    stats["onset_x"] = onset_x
+    stats["offset_x"] = offset_x
+    stats["width"] = offset_x - onset_x
+
+    # intersections points left and right
+    # is the point between onset and apex where the prototype crosses the baseline
+    # is the point between apex and offset where the prototype crosses the baseline
+    ips_left = (apex_location + onset_x) // 2
+    ips_right = (apex_location + offset_x) // 2
+    stats["ips_left"] = ips_left
+    stats["ips_right"] = ips_right
+    stats["internal_width"] = ips_right - ips_left
+
+    onset_y = interval[onset_x]
+    offset_y = interval[offset_x]
+    stats["onset_y"] = onset_y
+    stats["offset_y"] = offset_y
+
+    return stats
--- src/espbm/plot.py
+++ src/espbm/plot.py
@@ -6,10 +6,11 @@ from matplotlib import figure
 
 from .curlyBrace import curlyBrace
 
+
 def ear_time_series(
     ear_r: np.ndarray,
-    ear_l: np.ndarray, 
-    xmin: int | None = None, 
+    ear_l: np.ndarray,
+    xmin: int | None = None,
     xmax: int | None = None,
 ) -> tuple[figure.Figure, np.ndarray]:
     """
@@ -34,12 +35,12 @@ def ear_time_series(
         raise ValueError("ear_r must be a 1D array")
     if ear_l.ndim != 1:
         raise ValueError("ear_l must be a 1D array")
-    
+
     if xmin is not None and not isinstance(xmin, int):
         raise TypeError("xmin must be an integer")
     if xmax is not None and not isinstance(xmax, int):
         raise TypeError("xmax must be an integer")
-    
+
     if xmin is None:
         xmin = 0
 
@@ -48,32 +49,32 @@ def ear_time_series(
     elif xmax > max(len(ear_r), len(ear_l)):
         xmax = max(len(ear_r), len(ear_l))
 
-    fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(20, 6), sharex=True, sharey=True, gridspec_kw={'hspace': 0.1})
-    
-    axs[0].plot(ear_r, c='red',  label='Right Eye [EAR Value]')
-    axs[1].plot(ear_l, c='blue', label='Left Eye [EAR Value]')
+    fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(20, 6), sharex=True, sharey=True, gridspec_kw={"hspace": 0.1})
+
+    axs[0].plot(ear_r, c="red", label="Right Eye [EAR Value]")
+    axs[1].plot(ear_l, c="blue", label="Left Eye [EAR Value]")
 
     axs[0].minorticks_on()
     axs[0].set_xlim([xmin, xmax])
-    axs[1].set_xlabel('Frame [#]', fontsize="18")
-    
-    axs[0].set_ylabel('Right Eye [EAR Value]', fontsize="12")
-    axs[1].set_ylabel('Left Eye [EAR Value]',  fontsize="12")
-    
+    axs[1].set_xlabel("Frame [#]", fontsize="18")
+
+    axs[0].set_ylabel("Right Eye [EAR Value]", fontsize="12")
+    axs[1].set_ylabel("Left Eye [EAR Value]", fontsize="12")
+
     fig.suptitle("Eye Aspect Ratio [EAR] Time Series", fontsize="20")
     return fig, axs
 
-    
+
 def candidates_overview(candidates: list[np.ndarray] | np.ndarray) -> figure.Figure:
     """
     Generate a figure showing the overview of candidates' EAR time series.
-    
+
     Parameters:
         candidates (list[np.ndarray] | np.ndarray): A list of numpy arrays or a single numpy array representing the EAR time series of candidates.
-        
+
     Returns:
         figure.Figure: The generated matplotlib figure object.
-        
+
     Raises:
         TypeError: If candidates is not a list of numpy arrays.
     """
@@ -81,22 +82,22 @@ def candidates_overview(candidates: list[np.ndarray] | np.ndarray) -> figure.Fig
         candidates = [candidates]
     if not isinstance(candidates, list):
         raise TypeError("candidates must be a list of numpy arrays")
-    
+
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 7))
-    
+
     for idx, candidate in enumerate(candidates):
-        ax.plot(candidate, label=f"Candidate {idx+1}")
-        
-    ax.set_xlabel('Frame [#]', fontsize="18")
-    ax.set_ylabel('EAR Value', fontsize="18")
-    
+        ax.plot(candidate, label=f"Candidate {idx + 1}")
+
+    ax.set_xlabel("Frame [#]", fontsize="18")
+    ax.set_ylabel("EAR Value", fontsize="18")
+
     ax.set_ylim([0, 0.5])
-    
+
     ax.legend()
     fig.suptitle(f"Top {len(candidates)} candidates from EAR time series", fontsize="20")
     return fig
 
-    
+
 def matches(
     ear_l: np.ndarray,
     ear_r: np.ndarray,
@@ -120,17 +121,17 @@ def matches(
         figure.Figure: The matplotlib figure object.
     """
     fig, axs = ear_time_series(ear_r, ear_l, xmin, xmax)
-    
+
     for match in matches_l:
-        axs[0].axvspan(match[0], match[1], color='green', alpha=0.3)
+        axs[0].axvspan(match[0], match[1], color="green", alpha=0.3)
     for match in matches_r:
-        axs[1].axvspan(match[0], match[1], color='green', alpha=0.3)
+        axs[1].axvspan(match[0], match[1], color="green", alpha=0.3)
     return fig
 
-    
+
 def manual_prototype(
-    prototype: np.ndarray, 
-    xmin: int | None = None, 
+    prototype: np.ndarray,
+    xmin: int | None = None,
     xmax: int | None = None,
     params: tuple[float, float, float, float, int] | None = None,
 ) -> figure.Figure:
@@ -147,41 +148,43 @@ def manual_prototype(
     """
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 7))
     ax.plot(prototype)
-    ax.set_xlabel('Frame [#]', fontsize="18")
-    ax.set_ylabel('EAR Value', fontsize="18")
+    ax.set_xlabel("Frame [#]", fontsize="18")
+    ax.set_ylabel("EAR Value", fontsize="18")
     ax.set_ylim([0, 0.5])
     ax.set_xlim([xmin, xmax])
     fig.suptitle("Manual Prototype Pattern", fontsize="20")
-    
+
+    x_len = 100
     if params is None:
         return fig
-    
+
     sig1, sig2, bs, prominance, apex_location = params
 
     # draw a point for onset and offset
-    onset_x  = apex_location - int(sig1 * 3)
+    apex_location = int(apex_location * x_len)
+    apex_x = apex_location
+    apex_y = prototype[apex_x]
+    onset_x = apex_location - int(sig1 * 3)
     offset_x = apex_location + int(sig2 * 3)
-    apex_x   = apex_location
-    apex_y   = prototype[apex_x] 
-    
-    ax.plot(onset_x,  prototype[onset_x],  'ro')
-    ax.plot(offset_x, prototype[offset_x], 'ro')
-    ax.plot(apex_x,   prototype[apex_x],   'ro')
-    
+
+    ax.plot(onset_x, prototype[onset_x], "ro")
+    ax.plot(offset_x, prototype[offset_x], "ro")
+    ax.plot(apex_x, prototype[apex_x], "ro")
+
     # write onset, apex, and offset
     # slight lower left of the point
-    ax.text(onset_x-5,  prototype[onset_x]-0.02,  'Onset', fontsize=12, color='r')
-    ax.text(offset_x, prototype[offset_x]-0.02, 'Offset', fontsize=12, color='r')
-    ax.text(apex_x,   apex_y-0.02,   'Apex', fontsize=12, color='r')
-    
+    ax.text(onset_x - 5, prototype[onset_x] - 0.02, "Onset", fontsize=12, color="r")
+    ax.text(offset_x, prototype[offset_x] - 0.02, "Offset", fontsize=12, color="r")
+    ax.text(apex_x, apex_y - 0.02, "Apex", fontsize=12, color="r")
+
     # write the text prominance to the vertical line
-    ax.text(apex_x-3, apex_y+prominance/3, 'Prominance', fontsize=12, color='r', rotation=90)
-    
+    ax.text(apex_x - 3, apex_y + prominance / 3, "Prominance", fontsize=12, color="r", rotation=90)
+
     # draw lines to describe the prototype
-    ax.vlines(x=apex_x, ymin=prototype[apex_location], ymax=bs, color='r', linestyle='--')
-    ax.hlines(y=bs, xmin=onset_x, xmax=offset_x, color='r', linestyle='--')
-    
+    ax.vlines(x=apex_x, ymin=prototype[apex_location], ymax=bs, color="r", linestyle="--")
+    ax.hlines(y=bs, xmin=onset_x, xmax=offset_x, color="r", linestyle="--")
+
     # draw curly braces
-    curlyBrace(fig, ax, (onset_x, bs), (apex_x, bs), 0.03,  bool_auto=True, c="r",  str_text="3 * σ1")
-    curlyBrace(fig, ax, (apex_x, bs), (offset_x, bs),0.03, bool_auto=True, c="r", str_text="3 * σ2")
+    curlyBrace(fig, ax, (onset_x, bs), (apex_x, bs), 0.03, bool_auto=True, c="r", str_text="3 * σ1")
+    curlyBrace(fig, ax, (apex_x, bs), (offset_x, bs), 0.03, bool_auto=True, c="r", str_text="3 * σ2")
     return fig
